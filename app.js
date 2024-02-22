b63d3801e5620951f0b3397506b9d59f2e7a16d0
Nguyen Minh Tan <99700061+tantahi28@users.noreply.github.com>
2024-02-22 11:07:42 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
